5e4d0ee14631cc8f493632aac0ae3fb7c3ddfd19
KishorAO <kishor.phawade@anchanto.com>
2021-07-24 13:05:21 +0530

    array methods

--- src/app/javascript-array-methods/javascript-array-methods.component.ts
+++ src/app/javascript-array-methods/javascript-array-methods.component.ts
@@ -73,6 +73,130 @@ console.log(sumOfAge,'sumOfAge');
     },{})
 console.log(checkSameAge,'checkSameAge');
 
-  }
+// Found
+
+const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find(element => element > 5);
+
+console.log(found);
+// expected output: 12
+
+
+//  Includes
+
+// The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate
+
+
+const arrayInclude = [1, 2, 3];
+
+console.log(array1.includes(2));
+// expected output: true
+
+const pets = ['cat', 'dog', 'bat'];
+
+console.log(pets.includes('cat'));
+// expected output: true
+
+console.log(pets.includes('at'));
+// expected output: false
+
+// Index of
+
+// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+console.log(beasts.indexOf('bison'));
+// expected output: 1
+
+// start from index 2
+console.log(beasts.indexOf('bison', 2));
+// expected output: 4
+
+console.log(beasts.indexOf('giraffe'));
+// expected output: -1
+
+// findIndex
+
+// The findIndex() method returns the index of the first element in the array that satisfies the provided testing function. Otherwise, it returns -1, indicating that no element passed the test.
+
+const arrayfindIndex = [5, 12, 8, 130, 44];
+
+const isLargeNumber = (element) => element > 13;
+
+console.log(array1.findIndex(isLargeNumber));
+// expected output: 3
+
+
+let fruits = ['Apple', 'Banana']
+
+console.log(fruits.length)
+
+let first = fruits[0]
+// Apple
+
+let last = fruits[fruits.length - 1]
+// Banana
+
+fruits.forEach(function(item, index, array) {
+  console.log(item, index)
+})
+// Apple 0
+// Banana 1
+
+let newLength = fruits.push('Orange')
+// ["Apple", "Banana", "Orange"]
+
+let lasts = fruits.pop() // remove Orange (from the end)
+// ["Apple", "Banana"]
+
+let firsts = fruits.shift() // remove Apple from the front
+// ["Banana"]
+
+let newLengths = fruits.unshift('Strawberry') // add to the front
+// ["Strawberry", "Banana"]
+
+let pos = fruits.indexOf('Banana')
+// 1
+
+let removedItem = fruits.splice(pos, 1) // this is how to remove an item
+
+// ["Strawberry", "Mango"]
+
+let vegetables = ['Cabbage', 'Turnip', 'Radish', 'Carrot']
+console.log(vegetables)
+// ["Cabbage", "Turnip", "Radish", "Carrot"]
+
+let poss = 1
+let n = 2
+
+let removedItems = vegetables.splice(pos, n)
+// this is how to remove items, n defines the number of items to be removed,
+// starting at the index position specified by pos and progressing toward the end of array.
+
+console.log(vegetables)
+// ["Cabbage", "Carrot"] (the original array is changed)
+
+console.log(removedItems)
+// ["Turnip", "Radish"]
+
+let shallowCopy = fruits.slice() // this is how to make a copy
+// ["Strawberry", "Mango"]
 
 }
+
+
+
+
+}
+
+
+ 
+
+
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+
+// Array Methods in JavaScript 
